bbaafa937a62bbcde66f1f976bbd3fda08c2af51
Froggo <froggocraft@gmail.com>
2025-12-28 21:30:29 +0530

    Modified homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="flex-col mt-32 h-screen">
-      <h1 className="flex justify-center text-3xl font-bold">I haven't made a homepage yet</h1>
-      <p className="flex justify-center text-xl">Who cares about homepage anyways?</p>
-      <div className="flex justify-center mt-4">
+    <div className="h-screen flex flex-col items-center justify-center text-center px-4" style={{ height: 'calc(100vh - 64px)' }}>
+      <h1 className="flex justify-center text-3xl font-bold">
+        Share & Discover Builds
+      </h1>
+      <p className="flex justify-center text-xl mt-4">Discover and share Minecraft schematics in one simple, <br /> organized archive built for the community.</p>
+      <div className="flex justify-center mt-8">
         <Link to="/posts" className="btn btn-primary">Explore posts</Link>
       </div>
     </div>
